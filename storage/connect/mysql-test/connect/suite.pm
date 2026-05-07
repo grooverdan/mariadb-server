abb5a468987b020f847f319aaf0cccc02c72073a
Daniel Black <daniel@mariadb.org>
2026-05-07 16:45:39 +1000

    MDEV-38838 Memory loss in CONNECT from handler::ha_rnd_init
    This was a leak in <=unixODBC 2.3.15 where a cache was allocated
    but not returned. Using the suite.pm for the connect engine
    we see if the connect engine is compiled with ASAN (that includes
    LeakSanitizer) and we check the unixODBC version to see if it is
    a version (currently unreleased) that has a inst_logClose symbol
    which is part of the unixODBC leak fix. If it is not a leak fixed
    version, a LD_PRELOAD of the unixODBC library occurs so that the
    LeakSanitizer suppression can resolve the save_ini_cache entry.
    
    Alterates considered:
    1. __clear_ini_cache in libodbcinst (2.3.12+) is an interface,
    for which there is no header define. A CMake would need to
    do full compile and link to check its existance. Workable,
    just wanted a simplier solution. It looked like it shoud be a
    better interface in unixODBC.
    
    2. could skip asan tests on CONNECT/ODBC, but this is skipping
    coverage we want.

--- storage/connect/mysql-test/connect/suite.pm
+++ storage/connect/mysql-test/connect/suite.pm
@@ -10,7 +10,63 @@ return "No CONNECT engine" unless $ENV{HA_CONNECT_SO} or
 return "Not run for embedded server" if $::opt_embedded_server and
                                         $ENV{HA_CONNECT_SO};
 
-sub is_default { 1 }                                  
+sub is_default { 1 }
+
+# To allow the lsan suppression on unixodbc to work
+# the llvm-symbolizer needs to be are of the address
+# resolution even after the HA_CONNECT_SO has been dlclosed.
+
+# Check OS and file existence
+if ($^O =~ /linux|darwin|unix/i && -x "/usr/bin/readelf")
+{
+  my $asan_symbols = 0;
+  my $file = $::plugindir . '/' .  $ENV{HA_CONNECT_SO};
+
+  # Open readelf -s output and scan for __asan symbols
+  open(my $sh, '-|', "readelf -s '$file'")
+      or die "Failed to run readelf: $!\n";
+  while (<$sh>) { $asan_symbols = 1 if /__asan/; }
+  close($sh);
+
+  if ($asan_symbols && -x "/usr/bin/ldd")
+  {
+    # To allow the lsan suppression on unixodbc to work
+    # the llvm-symbolizer needs to be aware of the address
+    # resolution even after the HA_CONNECT_SO has been dlclosed.
+
+    my $lib_path;
+    open(my $ldd, '-|', "ldd $file") or die "Failed to run ldd: $!";
+
+    while (<$ldd>)
+    {
+        chomp;
+        # Example ldd line: libodbc.so.2 => /usr/lib/x86_64-linux-gnu/libodbc.so.2 (0x00007f...)
+        if (/libodbc\.so(?:\.\d+)*\s+=>\s+(\S+)/)
+	{
+            $lib_path = $1;
+            last;  # stop after the first match
+        }
+    }
+    close $ldd;
+
+    # assuming odbcinst is in the same path so we can check
+    # if it has fixed version.
+    my $libodbcinst = $lib_path;
+    $libodbcinst =~ s/odbc/odbcinst/;
+    my $leakfixed= 0;
+    if ( -f $libodbcinst )
+    {
+      open(my $sh, '-|', "readelf -s '$libodbcinst'")
+          or die "Failed to run readelf: $!\n";
+      while (<$sh>) { $leakfixed = 1 if /inst_logClose/; }
+      close($sh);
+    }
+    if (!$leakfixed && $lib_path)
+    {
+      $ENV{LD_PRELOAD} = $ENV{LD_PRELOAD} ? "$ENV{LD_PRELOAD}:$lib_path" : $lib_path;
+    }
+  }
+}
 
 bless { };
 
